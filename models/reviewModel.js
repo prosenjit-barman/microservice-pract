d6c4e143ce0be3042b2232ab6af7f20a73c6bfc0
Prosenjit Barman <prosenjitbarmann@gmail.com>
2022-06-01 12:36:37 +0600

    Part 8 Chapter 11

---
+++ models/reviewModel.js
@@ -0,0 +1,39 @@
+//Review / Rating / Created At / ref to User / ref to Tour
+const mongoose = require('mongoose');
+const reviewSchema = new mongoose.Schema(
+    {
+        review: {
+            type: String,
+            require: [true, 'Review Can not be Empty!']
+        },
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tour',
+            required: [true, 'Review Must Belong to a Tour.']
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: [true, 'Review Must belong to a User']
+        }
+
+    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    });
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
+
+//Parent Referencing will be used in this matter.
