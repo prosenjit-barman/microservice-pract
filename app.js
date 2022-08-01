187897814426ddb5cc96d962cfa67487cac946d3
ProsenjitB-Xpeed <prosenjit@xpeedstudio.com>
2022-08-01 22:02:47 +0600

    bodyparser added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const compression = require('compression');
 
 const AppError = require('./utils/appError');
@@ -64,7 +65,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 app.post('/webhook-checkout', 
-  express.raw({ type: 'application/json' }),
+  bodyParser.raw({ type: 'application/json' }),
   bookingController.webhookCheckout
 )
 
