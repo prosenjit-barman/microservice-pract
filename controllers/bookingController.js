136d5983c914296f2daa8530ed288fdb6e565c1d
ProsenjitB-Xpeed <prosenjit@xpeedstudio.com>
2022-08-06 22:21:10 +0600

    Stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -64,7 +64,7 @@ exports.webhookCheckout = (req, res, next) => {
 
   if (event.type === 'checkout.session.completed') createBookingCheckout(event.data.object);
 
-  res.status(200).json({ received: true });
+  res.status(200).json({ recieved: true });
 };
 
 exports.createBooking = factory.createOne(Booking);
