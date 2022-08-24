30dba8c204fc048b092223748292f537ddd59418
ProsenjitB-Xpeed <prosenjit@xpeedstudio.com>
2022-08-24 23:59:15 +0600

    Dockerized.

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,14 +3,14 @@ const bookingController = require("./../controllers/bookingController");
 const authController = require("./../controllers/authController");
 const router = express.Router();
 //By default each router only has access to the params of their specific routes. In order to get access to other router, we need to use merge params
-router.use(authController.protect);
+//router.use(authController.protect);
 
-router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+//router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
 
 router.use(authController.restrictTo('admin', 'lead-guide'));
 
 router.route('/')
-.get(bookingController.getallBookings)
+.get(bookingController.getallBooking)
 .post(bookingController.createBooking);
 
 router.route('/:id')
